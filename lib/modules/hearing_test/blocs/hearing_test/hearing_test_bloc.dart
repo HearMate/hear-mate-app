c647dd7857955f46fb0cf47976a2c8e0575c1fe5
Wojciech Trapkowski <wojciechtrapkowski1@gmail.com>
2025-03-02 15:32:06 +0100

    Sound player.

--- lib/modules/hearing_test/blocs/hearing_test/hearing_test_bloc.dart
+++ lib/modules/hearing_test/blocs/hearing_test/hearing_test_bloc.dart
@@ -1,15 +1,22 @@
 import 'package:bloc/bloc.dart';
+import 'package:hear_mate_app/modules/hearing_test/repositories/hearing_test_sounds_player_repository.dart';
 import 'package:meta/meta.dart';
 
 part 'hearing_test_event.dart';
 part 'hearing_test_state.dart';
 
 class HearingTestBloc extends Bloc<HearingTestEvent, HearingTestState> {
+  final HearingTestSoundsPlayerRepository _soundsPlayerRepository;
+
   final List<double> testFrequencies = [1000, 500, 250, 125, 2000, 4000, 8000];
   int currentFrequencyIndex = 0;
   List<Map<String, dynamic>> testResults = [];
 
-  HearingTestBloc() : super(HearingTestState()) {
+  HearingTestBloc({
+    required HearingTestSoundsPlayerRepository
+    hearingTestSoundsPlayerRepository,
+  }) : _soundsPlayerRepository = hearingTestSoundsPlayerRepository,
+       super(HearingTestState()) {
     on<HearingTestStartTest>(_onStartTest);
     on<HearingTestButtonPressed>(_onButtonPressed);
     on<HearingTestButtonReleased>(_onButtonReleased);
@@ -21,19 +28,19 @@ class HearingTestBloc extends Bloc<HearingTestEvent, HearingTestState> {
   void _onStartTest(
     HearingTestStartTest event,
     Emitter<HearingTestState> emit,
-  ) {}
+  ) async {}
 
   void _onButtonPressed(
     HearingTestButtonPressed event,
     Emitter<HearingTestState> emit,
-  ) {
+  ) async {
     emit(state.copyWith(isButtonPressed: true));
   }
 
   void _onButtonReleased(
     HearingTestButtonReleased event,
     Emitter<HearingTestState> emit,
-  ) {
+  ) async {
     emit(state.copyWith(isButtonPressed: false));
   }
 
