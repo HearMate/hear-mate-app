cae73760c3006bfc625f8604bca7b3ad4429b757
Michał Tarnowski <michal.tarnowski03@gmail.com>
2025-04-01 00:57:47 +0200

    EchoParse welcome page done.

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -4,9 +4,16 @@ import FlutterMacOS
 class MainFlutterWindow: NSWindow {
   override func awakeFromNib() {
     let flutterViewController = FlutterViewController()
-    let windowFrame = self.frame
+    let windowFrame = self.frame // Pobiera domyślny rozmiar okna
     self.contentViewController = flutterViewController
     self.setFrame(windowFrame, display: true)
+
+    // Ustawienie minimalnego i maksymalnego rozmiaru na aktualny rozmiar okna
+    self.minSize = windowFrame.size
+    self.maxSize = windowFrame.size
+    
+    // Usunięcie możliwości zmiany rozmiaru
+    self.styleMask.remove(.resizable)
 
     RegisterGeneratedPlugins(registry: flutterViewController)
 
