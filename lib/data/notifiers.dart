83731ac5412bb43099ae8ed3d2e06d75c665c684
Michał Tarnowski <michal.tarnowski03@gmail.com>
2025-04-15 15:47:34 +0200

    Add language selection and locale management features
    - Implemented language support with English and Polish options.
    - Created LocaleProvider for managing locale state.
    - Added EchoParseSettingPage for language selection in settings.
    - Updated HMAppBar to navigate to settings and manage dark mode state.

--- lib/data/notifiers.dart
+++ lib/data/notifiers.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 
 ValueNotifier<bool> isDarkModeNotifier = ValueNotifier(false); 
+ValueNotifier<bool> isSettingPageOn = ValueNotifier(false);
--- lib/widgets/hm_app_bar.dart
+++ lib/widgets/hm_app_bar.dart
@@ -1,16 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:hear_mate_app/data/notifiers.dart';
+import 'package:hear_mate_app/widgets/locale_provider.dart';
 
-class HMAppBar extends StatelessWidget implements PreferredSizeWidget {
+class HMAppBar extends StatefulWidget implements PreferredSizeWidget {
   const HMAppBar({super.key, required this.title});
 
   final String title;
 
   @override
+  State<HMAppBar> createState() => _HMAppBarState();
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
+
+class _HMAppBarState extends State<HMAppBar> {
+  @override
   Widget build(BuildContext context) {
+
     return AppBar(
-      title: Text(title, style: TextStyle()),
+      title: Text(widget.title, style: TextStyle()),
+      
       actions: [
+        
         IconButton(
           onPressed: () {
             isDarkModeNotifier.value = !isDarkModeNotifier.value;
@@ -22,14 +34,26 @@ class HMAppBar extends StatelessWidget implements PreferredSizeWidget {
             },
           ),
         ),
-        Padding(
-          padding: const EdgeInsets.fromLTRB(0, 0, 8.0, 0),
-          child: IconButton(onPressed: () {}, icon: Icon(Icons.settings)),
+        ValueListenableBuilder(
+          valueListenable: isSettingPageOn,
+          builder: (context, isSettingPage, child) {
+            if (!isSettingPage) {
+              return Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 8.0, 0),
+                child: IconButton(
+                  onPressed: () {
+                    isSettingPageOn.value = !isSettingPageOn.value;
+                    Navigator.pushNamed(context, '/echo_parse/settings');
+                  },
+                  icon: Icon(Icons.settings),
+                ),
+              );
+            } else {
+              return Padding(padding: const EdgeInsets.fromLTRB(0, 0, 8.0, 0));
+            }
+          },
         ),
       ],
     );
   }
-
-  @override
-  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 }
