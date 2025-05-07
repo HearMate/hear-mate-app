c9bdcde65439b56129a40f909eec7d71270f3253
Michał Tarnowski <michal.tarnowski03@gmail.com>
2025-05-07 17:06:51 +0200

    Update About page layout and improve localization strings

--- lib/screens/about_page.dart
+++ lib/screens/about_page.dart
@@ -18,128 +18,135 @@ class AboutPage extends StatelessWidget {
         route: ModalRoute.of(context)?.settings.name ?? "",
       ),
       body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(24.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Lottie.asset("assets/lotties/welcome.json", height: 100),
-              Text(
-                "HearMate",
-                style: Theme.of(context).textTheme.headlineLarge,
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(height: 16),
-
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  loc.about_hearmate,
-                  textAlign: TextAlign.justify,
-                  style: Theme.of(context).textTheme.bodyLarge,
-                ),
-              ),
-              const SizedBox(height: 32),
-
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 40.0),
-                child: Lottie.asset(
-                  "assets/lotties/echoparse_welcome.json",
-                  height: MediaQuery.of(context).size.width > 500 ? 150 : 100,
-                ),
-              ),
-              Text(
-                "EchoParse",
-                style: Theme.of(context).textTheme.headlineLarge,
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(height: 16),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  loc.about_echoparse,
-                  textAlign: TextAlign.justify,
-                  style: Theme.of(context).textTheme.bodyLarge,
-                ),
-              ),
-              const SizedBox(height: 32),
-
-              Text(
-                loc.about_projectTeam,
-                style: Theme.of(context).textTheme.headlineSmall,
-              ),
-              const SizedBox(height: 16),
-
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+        child: Center(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              maxWidth: MediaQuery.of(context).size.width > 500 ? 600 : double.infinity,
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
+                  Lottie.asset("assets/lotties/welcome.json", height: 100),
                   Text(
-                    '• prof. dr hab. inż. Andrzej Czyżewski',
-                    style: Theme.of(context).textTheme.bodyLarge,
+                    "HearMate",
+                    style: Theme.of(context).textTheme.headlineLarge,
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 16),
+
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      loc.about_hearmate,
+                      textAlign: TextAlign.justify,
+                      style: Theme.of(context).textTheme.bodyLarge,
+                    ),
+                  ),
+                  const SizedBox(height: 32),
+
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 40.0),
+                    child: Lottie.asset(
+                      "assets/lotties/echoparse_welcome.json",
+                      height: MediaQuery.of(context).size.width > 500 ? 150 : 100,
+                    ),
                   ),
                   Text(
-                    '• mgr Dominika Zagórska',
-                    style: Theme.of(context).textTheme.bodyLarge,
+                    "EchoParse",
+                    style: Theme.of(context).textTheme.headlineLarge,
+                    textAlign: TextAlign.center,
                   ),
-                  Text(
-                    '• mgr inż. Marina Galanina',
-                    style: Theme.of(context).textTheme.bodyLarge,
+                  const SizedBox(height: 16),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      loc.about_echoparse,
+                      textAlign: TextAlign.justify,
+                      style: Theme.of(context).textTheme.bodyLarge,
+                    ),
                   ),
+                  const SizedBox(height: 32),
+
                   Text(
-                    '• Michał Tarnowski',
-                    style: Theme.of(context).textTheme.bodyLarge,
+                    loc.about_projectTeam,
+                    style: Theme.of(context).textTheme.headlineSmall,
                   ),
-                  Text(
-                    '• Wojciech Trapkowski',
-                    style: Theme.of(context).textTheme.bodyLarge,
+                  const SizedBox(height: 16),
+
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '• prof. dr hab. inż. Andrzej Czyżewski',
+                        style: Theme.of(context).textTheme.bodyLarge,
+                      ),
+                      Text(
+                        '• mgr Dominika Zagórska',
+                        style: Theme.of(context).textTheme.bodyLarge,
+                      ),
+                      Text(
+                        '• mgr inż. Marina Galanina',
+                        style: Theme.of(context).textTheme.bodyLarge,
+                      ),
+                      Text(
+                        '• Michał Tarnowski',
+                        style: Theme.of(context).textTheme.bodyLarge,
+                      ),
+                      Text(
+                        '• Wojciech Trapkowski',
+                        style: Theme.of(context).textTheme.bodyLarge,
+                      ),
+                      Text(
+                        '• Stanisław Grochowski',
+                        style: Theme.of(context).textTheme.bodyLarge,
+                      ),
+                      Text(
+                        '• Dawid Glazik',
+                        style: Theme.of(context).textTheme.bodyLarge,
+                      ),
+                    ],
                   ),
-                  Text(
-                    '• Stanisław Grochowski',
-                    style: Theme.of(context).textTheme.bodyLarge,
+                  const SizedBox(height: 48),
+                  Center(
+                    child: BlocBuilder<HMThemeBloc, HMThemeState>(
+                      builder:(context, themeState) {
+                        return themeState.isDarkMode
+                            ? Image.asset(
+                              'assets/images/pg_logo_darkmode.png',
+                              height: 150,
+                            )
+                            : Image.asset('assets/images/pg_logo.png', height: 150);
+                      },
+                    ),
                   ),
+                  const SizedBox(height: 32),
+                  Center(
+                    child: BlocBuilder<HMThemeBloc, HMThemeState>(
+                      builder:(context, themeState) {
+                        return themeState.isDarkMode
+                            ? Image.asset(
+                              'assets/images/ifips_logo_darkmode.png',
+                              height: 150,
+                            )
+                            : Image.asset(
+                              'assets/images/ifips_logo.png',
+                              height: 150,
+                            );
+                      },
+                    ),
+                  ),
+                  const SizedBox(height: 48),
+
                   Text(
-                    '• Dawid Glazik',
-                    style: Theme.of(context).textTheme.bodyLarge,
+                    '© ${DateTime.now().year} HearMate – Politechnika Gdańska',
+                    style: Theme.of(context).textTheme.bodySmall,
+                    textAlign: TextAlign.center,
                   ),
                 ],
               ),
-              const SizedBox(height: 48),
-              Center(
-                child: BlocBuilder<HMThemeBloc, HMThemeState>(
-                  builder:(context, themeState) {
-                    return themeState.isDarkMode
-                        ? Image.asset(
-                          'assets/images/pg_logo_darkmode.png',
-                          height: 150,
-                        )
-                        : Image.asset('assets/images/pg_logo.png', height: 150);
-                  },
-                ),
-              ),
-              const SizedBox(height: 32),
-              Center(
-                child: BlocBuilder<HMThemeBloc, HMThemeState>(
-                  builder:(context, themeState) {
-                    return themeState.isDarkMode
-                        ? Image.asset(
-                          'assets/images/ifips_logo_darkmode.png',
-                          height: 150,
-                        )
-                        : Image.asset(
-                          'assets/images/ifips_logo.png',
-                          height: 150,
-                        );
-                  },
-                ),
-              ),
-              const SizedBox(height: 48),
-
-              Text(
-                '© ${DateTime.now().year} HearMate – Politechnika Gdańska',
-                style: Theme.of(context).textTheme.bodySmall,
-                textAlign: TextAlign.center,
-              ),
-            ],
+            ),
           ),
         ),
       ),
