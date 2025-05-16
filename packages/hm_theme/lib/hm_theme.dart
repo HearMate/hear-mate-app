993865347756ce47a534d045527f8c2fbd2198c6
Michał Tarnowski <michal.tarnowski03@gmail.com>
2025-05-16 17:36:08 +0200

    Changed themes file for the app ui enhancement

--- packages/hm_theme/lib/hm_theme.dart
+++ packages/hm_theme/lib/hm_theme.dart
@@ -2,44 +2,78 @@ import 'package:flutter/material.dart';
 
 export 'src/bloc/hm_theme_bloc.dart';
 
+class AppColors {
+  static const red = Color(0xFFF94F46);
+  static const green = Colors.teal;
+  static const white = Colors.white;
+}
+
+
 ThemeData buildHearMateTheme({required bool isDarkMode}) {
-  const echoParseRed = Color(0xFFF94F46);
 
   const TextTheme customTextTheme = TextTheme(
     displayLarge: TextStyle(
-      fontSize: 64.0,
-      fontWeight: FontWeight.bold,
-    ),
-    labelLarge: TextStyle(
-      fontSize: 20.0,
-      color: Colors.white,
       fontWeight: FontWeight.bold,
     ),
     displayMedium: TextStyle(
-      fontSize: 32.0,
       fontWeight: FontWeight.bold,
+    ),
+    labelLarge: TextStyle(
+      fontWeight: FontWeight.bold,
+      fontSize: 18.0
     ),
     bodyMedium: TextStyle(fontSize: 16.0),
   );
 
+  final colorScheme = ColorScheme.fromSeed(
+    seedColor: AppColors.green,
+    brightness: isDarkMode ? Brightness.dark : Brightness.light,
+  );
+
   return ThemeData(
-    colorScheme: ColorScheme.fromSeed(
-      seedColor: echoParseRed,
-      brightness: isDarkMode ? Brightness.dark : Brightness.light,
-      primary: echoParseRed,
+    useMaterial3: true, // ✅ Enables Material 3
+    colorScheme: colorScheme,
+    textTheme: customTextTheme.apply(
+      bodyColor: colorScheme.onSurface,
+      displayColor: colorScheme.onSurface,
     ),
-    textTheme: customTextTheme,
     filledButtonTheme: FilledButtonThemeData(
-      style: ElevatedButton.styleFrom(
-        minimumSize: const Size(252, 48),
-        padding: const EdgeInsets.all(8.0),
-        backgroundColor: echoParseRed,
-      ),
+      style: defaultFilledButtonStyle(colorScheme)
     ),
     outlinedButtonTheme: OutlinedButtonThemeData(
-      style: OutlinedButton.styleFrom(
-        minimumSize: const Size(252, 48),
+      style: ButtonStyle(
+        minimumSize: WidgetStateProperty.all(const Size(252, 48)),
+        foregroundColor: WidgetStateProperty.all(colorScheme.primary),
+        side: WidgetStateProperty.all(
+          BorderSide(color: colorScheme.primary),
+        ),
       ),
     ),
+  );
+}
+
+ButtonStyle attentionFilledButtonStyle(ColorScheme colorScheme) {
+  return ButtonStyle(
+    minimumSize: WidgetStateProperty.all(const Size(252, 48)),
+    padding: WidgetStateProperty.all(const EdgeInsets.all(16.0)),
+    backgroundColor: WidgetStateProperty.all(AppColors.red),
+    foregroundColor: WidgetStateProperty.all(AppColors.white),
+    textStyle: WidgetStateProperty.all(const TextStyle(
+      fontWeight: FontWeight.bold,
+      fontSize: 18.0,
+    )),
+  );
+}
+
+ButtonStyle defaultFilledButtonStyle(ColorScheme colorScheme) {
+  return ButtonStyle(
+    minimumSize: WidgetStateProperty.all(const Size(252, 48)),
+    padding: WidgetStateProperty.all(const EdgeInsets.all(8.0)),
+    backgroundColor: WidgetStateProperty.all(colorScheme.primary),
+    foregroundColor: WidgetStateProperty.all(colorScheme.onPrimary),
+    textStyle: WidgetStateProperty.all(const TextStyle(
+      fontWeight: FontWeight.bold,
+      fontSize: 18.0,
+    )),
   );
 }
