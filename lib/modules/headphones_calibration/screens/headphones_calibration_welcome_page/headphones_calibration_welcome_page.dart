517f70c203f7452e1ac2153bf95a5b660c9c5d68
Wojciech Trapkowski <wojciechtrapkowski1@gmail.com>
2025-09-09 22:58:21 +0200

    Last fixes.

--- lib/modules/headphones_calibration/screens/headphones_calibration_welcome_page/headphones_calibration_welcome_page.dart
+++ lib/modules/headphones_calibration/screens/headphones_calibration_welcome_page/headphones_calibration_welcome_page.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:hear_mate_app/features/headphones_search_bar/cubits/headphones_search_bar/headphones_search_bar_cubit.dart';
+import 'package:hear_mate_app/features/headphones_search/cubits/headphones_search_bar/headphones_search_bar_cubit.dart';
+import 'package:hear_mate_app/features/headphones_search/models/headphones_model.dart';
+import 'package:hear_mate_app/features/headphones_search/screens/headphones_search_bar.dart';
 import 'package:hear_mate_app/modules/headphones_calibration/blocs/headphones_calibration_module/headphones_calibration_module_bloc.dart';
-import 'package:hear_mate_app/modules/headphones_calibration/models/headphones_model.dart';
-import 'package:hear_mate_app/modules/headphones_calibration/models/headphones_search_result.dart';
 import 'package:hear_mate_app/features/headphones_search_bar/repositories/headphones_searcher_repository.dart';
-import 'package:hear_mate_app/features/headphones_search_bar/screens/headphones_search_bar.dart';
 import 'package:hear_mate_app/shared/widgets/hm_app_bar.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
